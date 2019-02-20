b1013218a0d163c7aebbd730a72ae4b2b1f0def9
Ovidio R. Calvet <calveto@gmail.com>
2019-02-20 07:11:17 -0500

    Gather script information

--- script.sh
+++ script.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+pid=$$
+script_name=$0
+user_name=$USER
+echo "Hello $user_name. You are running '$script_name' with pid '$pid'"
+
 say() {
     echo "Hello  World!"
 }
@@ -12,3 +18,5 @@ all_params=$@
 echo "I got $total_params params in the script"
 echo "First parameter in the script is $first_param"
 echo "Raw params are '$all_params'"
+
+ls -lart
