451f46ad7168fa934c4d0538b7e9230ef8c18994
Ovidio R. Calvet <calveto@gmail.com>
2019-02-20 07:07:18 -0500

    Evaluate script params

--- script.sh
+++ script.sh
@@ -4,3 +4,11 @@ say() {
 }
 
 say
+
+total_params=$#
+first_param=$1
+all_params=$@
+
+echo "I got $total_params params in the script"
+echo "First parameter in the script is $first_param"
+echo "Raw params are '$all_params'"
