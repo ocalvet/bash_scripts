173cbf5f2932495d882f444d01f1b778decaaf1f
Ovidio R. Calvet <calveto@gmail.com>
2019-02-22 03:35:31 -0500

    Add sample menu

--- script.sh
+++ script.sh
@@ -53,3 +53,15 @@ do
     echo $counter
     ((counter++))
 done
+
+echo -e "\nMENU"
+names='Kyle Cartman Stan Quit'
+PS3='Select character: '
+select name in $names
+do
+    if [ $name == 'Quit' ]
+    then
+        break
+    fi
+    echo Hello $name
+done
